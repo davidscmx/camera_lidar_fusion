333f139a266603b1fdb3ae189ba40552aa3e7882
David Sosa <david.sosa@protonmail.com>
2023-03-19 21:34:38 +0100

    Testing git lfs with tfrecord file 2

--- tests/waymo_reader_test.py
+++ tests/waymo_reader_test.py
@@ -1,11 +1,22 @@
 
+import git
 import sys
+from pathlib import Path
 from unittest import TestCase
 from simple_waymo_open_dataset_reader import WaymoDataFileReader
 
 
 class ImportSimpleWaymoDatasetReader(TestCase):
-    def test_imported_correctly(self):
-        print()
-        assert "simple_waymo_open_dataset_reader" in sys.modules.keys(), \
-            "simple_waymo_open_dataset_reader not found"
+
+    def setUp(self):
+
+        tf_rec = "training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord"
+        git_repo = git.Repo("./", search_parent_directories=True)
+        self.test_tf_record = Path(git_repo.working_dir) / "waymo_data" / tf_rec
+
+    def test_count_frames(self):
+        datafile = WaymoDataFileReader(self.test_tf_record)
+        table = datafile.get_record_table()
+        num_frames = len(table)
+        self.assertEqual(num_frames, 199)
+        datafile.file.close()
