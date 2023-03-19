22d6b8df3df51036077df9db46eba49c7cd4c8a9
David Sosa <david.sosa@protonmail.com>
2023-03-19 22:10:47 +0100

    Added small tfrecord for unit test

--- tests/waymo_reader_test.py
+++ tests/waymo_reader_test.py
@@ -11,7 +11,7 @@ class ImportSimpleWaymoDatasetReader(TestCase):
     def setUp(self):
 
         tf_rec = ("training_segment-10963653239323173269_1924_000_1944_000"
-                  "_with_camera_labels.tfrecord")
+                  "_with_camera_labels_part0.tfrecord")
 
         git_repo = git.Repo("./", search_parent_directories=True)
         self.test_tf_record = Path(git_repo.working_dir) / "waymo_data" / tf_rec
@@ -20,5 +20,5 @@ class ImportSimpleWaymoDatasetReader(TestCase):
         datafile = WaymoDataFileReader(self.test_tf_record)
         table = datafile.get_record_table()
         num_frames = len(table)
-        self.assertEqual(num_frames, 199)
+        self.assertEqual(num_frames, )
         datafile.file.close()
