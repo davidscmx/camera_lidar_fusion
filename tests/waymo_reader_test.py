8809fc5ce03277ec05570cd130de8e5dee71fd2c
David Sosa <david.sosa@protonmail.com>
2023-03-19 18:21:59 +0100

    Attempted to fix Waymo reader test 4

--- tests/waymo_reader_test.py
+++ tests/waymo_reader_test.py
@@ -1,9 +1,11 @@
 
 import sys
 from unittest import TestCase
+from simple_waymo_open_dataset_reader import WaymoDataFileReader
 
 
 class ImportSimpleWaymoDatasetReader(TestCase):
     def test_imported_correctly(self):
+        print()
         assert "simple_waymo_open_dataset_reader" in sys.modules.keys(), \
-            "simple_waymo_open_dataset_reader"
+            "simple_waymo_open_dataset_reader not found"
