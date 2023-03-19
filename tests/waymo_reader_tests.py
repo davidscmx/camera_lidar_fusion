6c33f81de06a09e75220750cc9bb5aedea653e2e
David Sosa <david.sosa@protonmail.com>
2023-03-19 15:00:08 +0100

    Added checkout and test for waymo reader

---
+++ tests/waymo_reader_tests.py
@@ -0,0 +1,9 @@
+
+import sys
+from unittest import TestCase
+
+
+class ImportSimpleWaymoDatasetReader(TestCase):
+    def test_imported_correctly(self):
+        assert "simple_waymo_open_dataset_reader" in sys.modules, \
+            "simple_waymo_open_dataset_reader not found"
